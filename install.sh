e6f32cc8fe085f9e4ddd75655eae319335a8b731
Nicholas Dietz <github@poleguy.com>
2025-06-19 16:03:13 -0500

    install start

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# run this with curl:
+# curl -sSL https://raw.githubusercontent.com/poleguy/protonpack/master/install.sh | bash
+
+git clone https://github.com/poleguy/protonpack.git
+
+cd protonpack
+
+# install vivado
+
+# install cvc64
+
+# install virtualenv stuff we might need
+
+# install python if needed
+
+# apt updates if needed
+
+# run
+
+./run_sim
