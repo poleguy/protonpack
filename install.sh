9d42ce0a7ec4afff12f526ca83dcc4d4c530a4cc
Nicholas Dietz <github@poleguy.com>
2025-06-20 00:41:47 -0500

    sendkeys to set up vm for initial automated testing

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # run this with curl:
 # curl -sSL https://raw.githubusercontent.com/poleguy/protonpack/master/install.sh | bash
 
+sudo apt install -y git
+
 git clone https://github.com/poleguy/protonpack.git
 
 cd protonpack
