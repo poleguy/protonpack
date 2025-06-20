1fd6d8317c56c643b53e6cd1fd72c9d3a419067b
Nicholas Dietz <github@poleguy.com>
2025-06-20 14:44:28 -0500

    update setup_python to fix virtualenv trouble

--- scripts/test_install.sh
+++ scripts/test_install.sh
@@ -7,18 +7,32 @@
 
 # do update 
 
+# turned off auto console to avoid needing to hit ctrl+] and wait for completion
+time sudo virt-install --name protonpack --memory 4096 --vcpu 2 --graphics vnc --osinfo ubuntu24.04 --disk size=100,backing_store=/var/lib/libvirt/iso/noble-server-cloudimg-amd64.img,bus=virtio --cloud-init user-data=scripts/user-data.yaml --noautoconsole
 
-time sudo virt-install --name protonpack --memory 4096 --vcpu 2 --graphics vnc --osinfo ubuntu24.04 --disk size=100,backing_store=/var/lib/libvirt/iso/noble-server-cloudimg-amd64.img,bus=virtio --cloud-init user-data=scripts/user-data.yaml
+# do nothing: send ctrl+] and you'll expect 
+# "Domain creation completed."
+
+#https://www.reddit.com/r/linuxadmin/comments/5gzdtk/properly_exiting_virtinstall/
+
+
 
 # do nothing: grep root password
 #PASS=J2lHiEj3jO4gb1lM
 
 # do nothing: grep ip address
 #[   15.038761] cloud-init[829]: ci-info: | enp1s0 | True |       192.168.122.12       | 255.255.255.0 | global | 52:54:00:44:49:9b |
-read -p "Enter ip from ci-info above: " IP
+#read -p "Enter ip from ci-info above: " IP
 
-# do nothing: send ctrl+] and you'll expect 
-# "Domain creation completed."
+IP = ""
+while [ ${#IP} -le 7 ]; do  # Loop while the length of 'result' is less than or equal to 7
+  IP=$(virsh domifaddr protonpack | sed -n '3p' | tr -s ' '| cut -d " " -f 5 | cut -d "/" -f 1)
+  sleep 0.5
+done
+
+
+echo "IP is $IP"
+
 
 # impressively fast!
 
@@ -31,7 +45,7 @@ read -p "Enter ip from ci-info above: " IP
 # no need with ssh key: virsh console protonpack
 
 
-ssh proton@$IP "wget -q -O - https://raw.githubusercontent.com/poleguy/protonpack/master/install.sh | bash"
+ssh -o StrictHostKeyChecking=no proton@$IP "wget -q -O - https://raw.githubusercontent.com/poleguy/protonpack/master/install.sh | bash"
 # do-nothing: accept fingerprint
 
 read -p "ARe you done?: " status
