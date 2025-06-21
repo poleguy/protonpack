a0a3c489f02261a3a67f423328c97bddd7b2c649
Nicholas Dietz <github@poleguy.com>
2025-06-21 10:27:52 -0500

    keep going if cleanup fails

--- scripts/test_install.sh
+++ scripts/test_install.sh
@@ -5,7 +5,14 @@ set -e
 # this is getting very meta, but how do I avoid the "it works on my machine" problem
 
 
+
+
 # do-nothing script for installation testing
+
+# destroy to start
+virsh destroy protonpack
+virsh undefine protonpack --remove-all-storage
+
 
 # do update 
 
@@ -26,7 +33,7 @@ time sudo virt-install --name protonpack --memory 4096 --vcpu 2 --graphics vnc -
 #[   15.038761] cloud-init[829]: ci-info: | enp1s0 | True |       192.168.122.12       | 255.255.255.0 | global | 52:54:00:44:49:9b |
 #read -p "Enter ip from ci-info above: " IP
 
-IP = ""
+IP=""
 while [ ${#IP} -le 7 ]; do  # Loop while the length of 'result' is less than or equal to 7
   IP=$(virsh domifaddr protonpack | sed -n '3p' | tr -s ' '| cut -d " " -f 5 | cut -d "/" -f 1)
   sleep 0.5
@@ -47,13 +54,13 @@ echo "IP is $IP"
 # no need with ssh key: virsh console protonpack
 
 # setup ssh
-ssh proton@192.168.122.40 ssh-keygen -t ed25519 -N "" -f /home/proton/.ssh/ubuntu
+ssh -o StrictHostKeyChecking=no proton@$IP "ssh-keygen -t ed25519 -N '' -f /home/proton/.ssh/ubuntu"
 # host should grab ubuntu.pub
-https://superuser.com/questions/429954/command-to-remove-a-ssh-authorized-key-on-server
+#https://superuser.com/questions/429954/command-to-remove-a-ssh-authorized-key-on-server
 temp_file=$(mktemp)
 grep -v "proton@ubuntu" ~/.ssh/authorized_keys > $temp_file
 cat $temp_file > ~/.ssh/authorized_keys && rm $temp_file
-ssh proton@192.168.122.40 cat /home/proton/.ssh/ubuntu.pub >> ~/.ssh/authorized_keys
+ssh -o StrictHostKeyChecking=no proton@$IP cat /home/proton/.ssh/ubuntu.pub >> ~/.ssh/authorized_keys
 
 ssh -o StrictHostKeyChecking=no proton@$IP "wget -q -O - https://raw.githubusercontent.com/poleguy/protonpack/master/install.sh | bash"
 # do-nothing: accept fingerprint
