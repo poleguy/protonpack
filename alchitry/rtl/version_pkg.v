0ac40791d734de29c59cb1423d929f64bb5253c2
Jenkins <jenkins@shure.com>
2025-10-29 03:04:22 -0500

    Product https://artifactory.shure.com/All_Shure_Components/ATLAS/protonpack/0.0.0.29/ Artifactory auto commit from build. main

--- alchitry/rtl/version_pkg.v
+++ alchitry/rtl/version_pkg.v
@@ -6,14 +6,14 @@ package version_pkg_v;
   localparam logic [7:0]  C_VERSION_MAJOR  = 8'd0;
   localparam logic [7:0]  C_VERSION_MINOR  = 8'd0;
   localparam logic [7:0]  C_VERSION_PATCH  = 8'd0;
-  localparam logic [7:0]  C_VERSION_BUILD  = 8'd25;
+  localparam logic [7:0]  C_VERSION_BUILD  = 8'd29;
 
   localparam logic [15:0] C_VERSION_YEAR   = 16'h2025;
   localparam logic [7:0]  C_VERSION_MONTH  = 8'h10;
-  localparam logic [7:0]  C_VERSION_DAY    = 8'h28;
-  localparam logic [7:0]  C_VERSION_HOUR   = 8'h13;
-  localparam logic [7:0]  C_VERSION_MINUTE = 8'h21;
-  localparam logic [7:0]  C_VERSION_SECOND = 8'h11;
+  localparam logic [7:0]  C_VERSION_DAY    = 8'h29;
+  localparam logic [7:0]  C_VERSION_HOUR   = 8'h03;
+  localparam logic [7:0]  C_VERSION_MINUTE = 8'h00;
+  localparam logic [7:0]  C_VERSION_SECOND = 8'h09;
   // END_VERSION_BLOCK
 
 endpackage : version_pkg_v
